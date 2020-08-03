4fc8859aab7e82e96128ea3f274aad6867b3a9bb
Randal <randal.t.krebs@gmail.com>
2020-08-03 20:46:05 -0300

    FinalizaçãoDia01

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import whatsAppIcon from "../../assets/images/icons/whatsapp.svg";
+import "./styles.css";
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars3.githubusercontent.com/u/68863261?s=460&u=75fafbe62297062a8a0a08bea06c3ebd5f0ffd94&v=4"
+          alt="Randal TK"
+        />
+        <div>
+          <strong>Randal TK</strong>
+          <span>Química</span>
+        </div>
+      </header>
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br /> <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsAppIcon} alt="WhatsApp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
