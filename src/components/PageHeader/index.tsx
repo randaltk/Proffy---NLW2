500f84e13d2aa85512d092600f0894d8021cf8ec
Randal <randal.t.krebs@gmail.com>
2020-08-12 15:55:00 -0300

    aula03

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -4,10 +4,11 @@ import { Link } from "react-router-dom";
 import backIcon from "./../../assets/images/icons/back.svg";
 import logoImg from "./../../assets/images/logo.svg";
 
-import "./style.css";
+import "./styles.css";
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -22,6 +23,9 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
       <div className="header-content">
         <strong>{props.title}</strong>
+
+        {props.description && <p>{props.description}</p>}
+
         {props.children}
       </div>
     </header>
--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,30 +2,35 @@ import React from "react";
 import whatsAppIcon from "../../assets/images/icons/whatsapp.svg";
 import "./styles.css";
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars3.githubusercontent.com/u/68863261?s=460&u=75fafbe62297062a8a0a08bea06c3ebd5f0ffd94&v=4"
-          alt="Randal TK"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Randal TK</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br /> <br />
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das
-        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
-        uma das minhas explosões.
-      </p>
+      <p>{teacher.bio}</p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
         <button type="button">
           <img src={whatsAppIcon} alt="WhatsApp" />
@@ -34,6 +39,6 @@ function TeacherItem() {
       </footer>
     </article>
   );
-}
+};
 
 export default TeacherItem;
